f9fa7cfd0a929b3b49800592882724e2e27df0ce
Nadejda <nadyash295923@gmail.com>
2024-04-12 23:42:12 +0300

    done power

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int result = numberToPrint;
+        if (power == 0 && numberToPrint !=0) System.out.print(1);
+        else if (numberToPrint == 0) System.out.print(0);
+        else{ for (int i = 1; i < power; i++)
+                result *= numberToPrint;
+                System.out.print(result);
+            }
     }
 
     public static void main(String[] args) {
