e8e8ffd9034e69ed6f8fe05ed57e5085263f088f
Nadejda <nadyash295923@gmail.com>
2024-04-12 23:57:13 +0300

    done p

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++)
+        {
+            for (int j = 0; j < cathetusLength-i;j++) System.out.print(" ");
+            for (int k = i; k >0; k-- ) System.out.print(k);
+            for (int k = 2; k <=i; k++ ) System.out.print(k);
+            System.out.println();
+        }
 
     }
 
